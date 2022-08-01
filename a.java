8e74ee5c36a0b6314148e6160315ae093823bada
007aakki <110383812+007aakki@users.noreply.github.com>
2022-08-01 14:26:38 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,22 @@
+package ritik;
+
+class a {
+	int a = 5, b=3 ,c=4;
+	static int d=5,e=6;
+	public static void main(System args[]) {
+		a ob= new a();
+		System.out.println(ob.a+ob.b+ob.c+d+e);
+		ob.a =ob.b+d;
+		ob.b=ob.c+e;
+		d= ob.a+ob.b;
+		System.out.println(ob.a+ob.b+ob.c+d+e);
+		a ob1 = new a();
+		ob1.a=ob.a+ob1.b;
+		ob1.b=ob1.a+ob1.c;
+		e=ob1.a+ob1.b;
+		System.out.println();
+		a ob2 =new a();
+		System.out.println(ob2.a+ob2.b+ob2.c+d+e);
+	}
+
+}
